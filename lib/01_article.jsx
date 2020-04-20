94a07c3abb5c9261cd51b4623bc47e9777335a02
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-20 14:17:09 +0200

    f implemented

--- lib/01_article.jsx
+++ lib/01_article.jsx
@@ -6,8 +6,8 @@ const Article = (props) => {
   return <div className="article"><h2>{props.title}</h2><p>{props.body}</p></div>;
 };
 
-const contain = document.querySelector('.container');
-ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, contain);
+// const contain = document.querySelector('.container');
+// ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, contain);
 // ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />);
 // TODO: export your Article component for tests
 export default Article;
